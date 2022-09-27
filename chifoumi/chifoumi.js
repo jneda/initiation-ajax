fc4e0921e17592211cea216b55ade8967a0b38b0
jneda <julien.dane@gmail.com>
2022-09-27 18:00:35 +0200

    Implémenté fonctions choix CPU et joueur + tests

--- chifoumi/chifoumi.js
+++ chifoumi/chifoumi.js
@@ -8,17 +8,65 @@ const NUL = "aucun";
 const JOUEUR = "joueur";
 const ORDINATEUR = "ordinateur";
 
+// fonction de choix CPU
+
+function choisirCPU() {
+
+  const choix = [PIERRE, FEUILLE, CISEAUX];
+  // on choisit au hasard un index entre 0 et 2
+  const index = Math.floor(Math.random() * 3);
+
+  return choix[index];
+}
+
+// fonction de choix joueur
+
+function choisirJoueur() {
+  // on définit les choix possibles
+  const possibilites = [
+    "p",
+    "pierre",
+    "f",
+    "feuille",
+    "c",
+    "ciseaux"
+  ];
+
+  // on boucle tant que l'utilisateur entre n'importe quoi
+  let choix;
+  do {
+    choix = prompt("Quel est ton choix ? ((p)ierre, (f)euille ou (c)iseaux)").toLowerCase();
+  } while (!possibilites.includes(choix));
+
+  // on s'assure de renvoyer une valeur utilisable
+  if (choix === "p") {
+    choix = PIERRE;
+  } else if (choix === "f") {
+    choix = FEUILLE;
+  } else if (choix === "c") {
+    choix = CISEAUX;
+  }
+
+  return choix;
+}
+
 // fonction de comparaison
 
-function comparer(choix1, choix2) {
-  if (choix1 === choix2) {
+function comparer(choixJoueur, choixCPU) {
+  if (choixJoueur === choixCPU) {
     return NUL;
   } else if (
-    (choix1 === PIERRE && choix2 === CISEAUX) ||
-    (choix1 === FEUILLE && choix2 === PIERRE) ||
-    (choix1 === CISEAUX && choix2 === FEUILLE)
+    (choixJoueur === PIERRE && choixCPU === CISEAUX) ||
+    (choixJoueur === FEUILLE && choixCPU === PIERRE) ||
+    (choixJoueur === CISEAUX && choixCPU === FEUILLE)
   ) {
     return JOUEUR;
   }
   return ORDINATEUR;
+}
+
+// fonction pour une manche
+
+function jouerManche() {
+
 }
--- chifoumi/chifoumi.test.js
+++ chifoumi/chifoumi.test.js
@@ -1,9 +1,48 @@
 // tests
 
-function testComparer(choix1, choix2, attendu) {
-  const resultat = comparer(choix1, choix2);
+// fonction choisirCPU()
+
+function testChoisirCPU(n) {
+  // on fait n choix et si tout se passe bien
+  // on devrait avoir à peu près le même nombre pour chaque option
+
+  const compteurs = [0, 0, 0]; // 0 = PIERRE, 1 = FEUILLE, 2 = CISEAUX
+
+  for (let i = 0; i < n; i++) {
+    const choix = choisirCPU();
+    if (choix === PIERRE) {
+      compteurs[0]++;
+    } else if (choix === FEUILLE) {
+      compteurs[1]++;
+    } else {
+      compteurs[2]++;
+    }
+  }
+
+  console.log(`Nombre de tirages : ${n}\n` +
+    `pierres : ${compteurs[0]}\n` +
+    `feuilles : ${compteurs[1]}\n` +
+    `ciseaux : ${compteurs[2]}`);
+}
+
+testChoisirCPU(1000);
+testChoisirCPU(10 * 1000);
+testChoisirCPU(100 * 1000);
+testChoisirCPU(1000 * 1000);
+
+// test choisirJoueur()
+// a la mano...
+
+while (prompt("Tester ?") !== "non") {
+  console.log(choisirJoueur());
+}
+
+// fonction comparer()
+
+function testComparer(choixJoueur, choixCPU, attendu) {
+  const resultat = comparer(choixJoueur, choixCPU);
   console.log(
-    `${choix1} vs ${choix2}\n=> vainqueur : ${resultat}` +
+    `${choixJoueur} vs ${choixCPU}\n=> vainqueur : ${resultat}` +
     ` vs attendu : ${attendu}\n=> OK : ${resultat === attendu ? true : false}`
   );
 }
