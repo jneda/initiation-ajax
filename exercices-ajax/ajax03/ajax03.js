145970491e8e02a8f3d3416a39502f10391058c6
jneda <julien.dane@gmail.com>
2022-10-20 09:49:19 +0200

    Amélioré illustration des changements d'état de XHR

--- exercices-ajax/ajax03/ajax03.js
+++ exercices-ajax/ajax03/ajax03.js
@@ -3,15 +3,16 @@ NB: le temps que prend la requête est variable (illustrer via outils de dév) *
 
 // on stocke la date de départ
 const startTime = Date.now();
-console.log(startTime);
+// console.log(startTime); // DEBUG
 
 // nouvelle instance XMLHttpRequest
 const xhr = new XMLHttpRequest();
 
-// parmaétrage de la requête
+// paramaétrage de la requête
 const methode = "GET";
 const _URL = "https://jsonplaceholder.typicode.com/users/1";
 
+// appel de la méthode open()
 xhr.open(methode, _URL);
 
 // definition de la fonction de callback pour
@@ -23,29 +24,30 @@ xhr.onreadystatechange = function () {
 
   let message;
 
+  message = `État n° ${xhr.readyState} - `;
   switch (xhr.readyState) {
     case XMLHttpRequest.UNSENT:
-      message = `Temps écoulé : ${Date.now() - startTime} ms - ` +
+      message += `Temps écoulé : ${Date.now() - startTime} ms - ` +
         "Le client a été créé. open() n'a pas encore été appelé.";
       break;
 
     case XMLHttpRequest.OPENED:
-      message = `Temps écoulé : ${Date.now() - startTime} ms - ` +
+      message += `Temps écoulé : ${Date.now() - startTime} ms - ` +
         "open()a été appelé.";
       break;
 
     case XMLHttpRequest.HEADERS_RECEIVED:
-      message = `Temps écoulé : ${Date.now() - startTime} ms - ` +
+      message += `Temps écoulé : ${Date.now() - startTime} ms - ` +
         "send() a été appelé, et les en-têtes et le statut sont disponibles.";
       break;
     
     case XMLHttpRequest.LOADING:
-      message = `Temps écoulé : ${Date.now() - startTime} ms - ` +
+      message += `Temps écoulé : ${Date.now() - startTime} ms - ` +
         "Téléchargement; responseText contient des données partielles. ";
       break;
 
     case XMLHttpRequest.DONE:
-      message = `Temps écoulé : ${Date.now() - startTime} ms - ` +
+      message += `Temps écoulé : ${Date.now() - startTime} ms - ` +
         "L'opération est terminée.";
   }
 
